68a67678074600bd5ac8de8074e9ececd5c31991
DURGESH <DURGESH@DURGESH-PC>
2018-09-05 09:58:04 -0700

    K2

--- Demo.java
+++ Demo.java
@@ -4,6 +4,12 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Hellow world");
+        display();
+        
+	}
+	static void display()
+	{
+		System.out.println("Hellow how r yu")
 	}
 
 }
